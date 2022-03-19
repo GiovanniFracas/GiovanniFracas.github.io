e25c66f64be9e88517f509b0b451cbbe3d20600a
Ilbubba <73846070+Ilbubba@users.noreply.github.com>
2022-03-19 12:00:05 +0100

    fadeinLeft

--- nascondi.js
+++ nascondi.js
@@ -9,8 +9,24 @@ $(() => {
     $('.card').on('mouseleave', function () {
         setTimeout(() => {
             console.log($(this).children(".card-text").fadeOut());
-        console.log($(this).children('.n1').fadeIn());
+            console.log($(this).children('.n1').fadeIn());
         }, 1000);
-        
+
     })
+
+    /*    $('#chisono').show(()=>{
+           $('#chisono').addClass('fadein')
+       })
+    */
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() + $(window).height() - 100 >= $("#team").offset().top) {
+            $('#team').addClass('fadeinRight')
+        }
+    })
+    $(window).on("scroll", function () {
+        if ($(window).scrollTop() + $(window).height() - 100 >= $("#chisono").offset().top) {
+            $('#chisono').addClass('fadeinLeft')
+        }
+    })
+   
 })
